5f60744bd83af07a445de89175f059e298f485c0
unknown <mastesUNG>
2019-11-07 17:02:11 +0700

    finish07112019New

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -134,7 +134,7 @@ class _HomeState extends State<Home> {
 
   Widget showAppName() {
     return Text(
-      'Ung Pinthong',
+      'Living And Facilities Co.,Ltd.',
       style: TextStyle(
         fontSize: MyStyle().h1,
         fontWeight: FontWeight.bold,
